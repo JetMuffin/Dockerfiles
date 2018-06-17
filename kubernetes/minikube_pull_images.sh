ceb08f6e9f7bd19f05e0ff2f08adbb9c8553aa01
JetMuffin <564936642@qq.com>
2018-06-17 14:41:43 +0800

    add apiserver
    Signed-off-by: JetMuffin <564936642@qq.com>

--- kubernetes/minikube_pull_images.sh
+++ kubernetes/minikube_pull_images.sh
@@ -2,8 +2,12 @@
 
 # kube-addon-manager
 docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-addon-manager:latest
-docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-addon-manager:latest k8s.gcr.ui/kube-addon-manager:v8.6
+docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-addon-manager:latest k8s.gcr.io/kube-addon-manager:v8.6
 
 # pause-amd64
 docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/pause-amd64:latest
 docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/pause-amd64:latest k8s.gcr.io/pause-amd64:3.1
+
+# apiserver-amd64
+docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-apiserver-amd64
+docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-addon-manager k8s.gcr.io/kube-apiserver-amd64:v1.10.0
