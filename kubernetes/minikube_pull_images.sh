45471046edf2411da66ff95f6ac36d21078b3e8d
JetMuffin <564936642@qq.com>
2018-06-17 15:46:25 +0800

    update scripts.
    Signed-off-by: JetMuffin <564936642@qq.com>

--- kubernetes/minikube_pull_images.sh
+++ kubernetes/minikube_pull_images.sh
@@ -17,19 +17,19 @@ docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-scheduler-amd64
 docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-scheduler-amd64 k8s.gcr.io/kube-scheduler-amd64:v1.10.0
 
 # sidecar
-docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/k8s-dns-sidecar:latest
-docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/k8s-dns-sidecar:latest k8s.gcr.io/k8s-dns-sidecar-amd64:1.14.5
+docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/k8s-dns-sidecar-amd64:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/k8s-dns-sidecar-amd64:latest k8s.gcr.io/k8s-dns-sidecar-amd64:1.14.5
 
 # storage-provisioner
 docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/storage-provisioner:latest
 docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/storage-provisioner:latest gcr.io/k8s-minikube/storage-provisioner:v1.8.1
 
 # k8s-dns-kube-dns
-docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/k8s-dns-kube-dns:latest
-docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/k8s-dns-kube-dns:latest k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.5
+docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/k8s-dns-kube-dnsamd-64:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/k8s-dns-kube-dns-amd64:latest k8s.gcr.io/k8s-dns-kube-dns-amd64:1.14.5
 
 # kube-dashboard
 # kubernetes-dashboard
-docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/kubernetes-dashboard:latest
-docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/kubernetes-dashboard:latest k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.1
+docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/kubernetes-dashboard-amd64:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/kubernetes-dashboard-amd64:latest k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.1
 
