2413394dcf50c26226c60ad9e2880d376d357b64
JetMuffin <564936642@qq.com>
2018-06-17 14:31:22 +0800

    update scripts.
    Signed-off-by: JetMuffin <564936642@qq.com>

--- kubernetes/minikube_pull_images.sh
+++ kubernetes/minikube_pull_images.sh
@@ -3,3 +3,7 @@
 # kube-addon-manager
 docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-addon-manager:latest
 docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/kube-addon-manager:latest k8s.gcr.ui/kube-addon-manager:v8.6
+
+# pause-amd64
+docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/pause-amd64:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/pause-amd64:latest k8s.gcr.io/pause-amd64:3.1
