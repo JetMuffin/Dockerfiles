132cf267733f2da4b3e7440cb9dcebf7a6f61610
JetMuffin <564936642@qq.com>
2018-06-21 21:57:06 +0800

    Add stastd.
    Signed-off-by: JetMuffin <564936642@qq.com>

--- kubeflow/minikube_pull_images.sh
+++ kubeflow/minikube_pull_images.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker pull registry.cn-hangzhou.aliyuncs/jetmuffin/ambassador:latest
-docker tag registry.cn-hangzhou.aliyuncs/jetmuffin/ambassador:latest quay.io/datawire/ambassador:0.30.1
+docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/ambassador:latest
+docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/ambassador:latest quay.io/datawire/ambassador:0.30.1
+
+docker pull registry.cn-hangzhou.aliyuncs.com/jetmuffin/statsd
+docker tag registry.cn-hangzhou.aliyuncs.com/jetmuffin/statsd quay.io/datawire/statsd:0.30.1
+
